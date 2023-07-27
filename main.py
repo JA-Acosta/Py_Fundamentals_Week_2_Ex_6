947823ac13a3f2a80505e725c4786d220957abd2
JAAR <joe.aaron.acosta@outlook.com>
2023-07-27 19:25:34 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 07/27/2023
+>>> Practicing Fundamentals Program 12
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that compares two lists after removing all duplicates and sorting them. If the lists are unique, the list will be merged such that they are still sorted. The program will then report the common elements between both lists as well as the unique elements. Finally, the program will report which values were duplicates in each individual list.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
